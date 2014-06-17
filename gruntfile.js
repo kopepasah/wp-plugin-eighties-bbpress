57731b61b1d637375c6b0a90640b3fa3424e5264
Justin Kopepasah <justin@kopepasah.com>
2014-06-17 22:21:20 +0900

    Add Grunt & Node stuff.

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		less: {
+			compress: {
+				files: {
+					'template/css/bbpress.min.css' : 'template/css/bbpress.less' 
+				},
+				options: {
+					compress: true,
+				}
+			},
+			standard: {
+				files: {
+					'template/css/bbpress.css' : 'template/css/bbpress.less'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['template/css/bbpress.less','template/less/*.less'],
+				tasks: ['less:compress'],
+				options: {
+					livereload: true,
+				}
+			}
+		}
+	});
+
+	// Load tasks
+	grunt.loadNpmTasks( 'grunt-contrib-less' );
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'less',
+		'watch'
+	]);
+};
