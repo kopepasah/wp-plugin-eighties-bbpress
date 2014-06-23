bb8fa5b43d45b22bd7db8bda5b9947fb4fef571d
Justin Kopepasah <justin@kopepasah.com>
2014-06-23 20:27:22 +0900

    Make the forums loop pretty.

--- template/bbpress/loop-single-forum.php
+++ template/bbpress/loop-single-forum.php
@@ -6,68 +6,67 @@
  */
 
 ?>
+<div class="bbp-forum-wrapper">
+	<ul id="bbp-forum-<?php bbp_forum_id(); ?>" <?php bbp_forum_class(); ?>>
 
-<ul id="bbp-forum-<?php bbp_forum_id(); ?>" <?php bbp_forum_class(); ?>>
+		<li class="bbp-forum-info bbp-info">
 
-	<li class="bbp-forum-info">
+			<?php if ( bbp_is_user_home() && bbp_is_subscriptions() ) : ?>
 
-		<?php if ( bbp_is_user_home() && bbp_is_subscriptions() ) : ?>
+				<span class="bbp-row-actions">
 
-			<span class="bbp-row-actions">
+					<?php do_action( 'bbp_theme_before_forum_subscription_action' ); ?>
 
-				<?php do_action( 'bbp_theme_before_forum_subscription_action' ); ?>
+					<?php bbp_forum_subscription_link( array( 'before' => '', 'subscribe' => '+', 'unsubscribe' => '&times;' ) ); ?>
 
-				<?php bbp_forum_subscription_link( array( 'before' => '', 'subscribe' => '+', 'unsubscribe' => '&times;' ) ); ?>
+					<?php do_action( 'bbp_theme_after_forum_subscription_action' ); ?>
 
-				<?php do_action( 'bbp_theme_after_forum_subscription_action' ); ?>
+				</span>
 
-			</span>
+			<?php endif; ?>
 
-		<?php endif; ?>
+			<?php do_action( 'bbp_theme_before_forum_title' ); ?>
 
-		<?php do_action( 'bbp_theme_before_forum_title' ); ?>
+			<h2 class="bbp-forum-title"><a href="<?php bbp_forum_permalink(); ?>"><?php bbp_forum_title(); ?></a></h2>
 
-		<a class="bbp-forum-title" href="<?php bbp_forum_permalink(); ?>"><?php bbp_forum_title(); ?></a>
+			<?php do_action( 'bbp_theme_after_forum_title' ); ?>
 
-		<?php do_action( 'bbp_theme_after_forum_title' ); ?>
+			<?php do_action( 'bbp_theme_before_forum_description' ); ?>
 
-		<?php do_action( 'bbp_theme_before_forum_description' ); ?>
+			<?php if ( bbp_get_forum_content() ) : ?>
+				<div class="bbp-forum-content"><?php bbp_forum_content(); ?></div>
+			<?php endif; ?>
 
-		<div class="bbp-forum-content"><?php bbp_forum_content(); ?></div>
+			<?php do_action( 'bbp_theme_after_forum_description' ); ?>
 
-		<?php do_action( 'bbp_theme_after_forum_description' ); ?>
+			<?php do_action( 'bbp_theme_before_forum_sub_forums' ); ?>
 
-		<?php do_action( 'bbp_theme_before_forum_sub_forums' ); ?>
+			<?php
+				$args = array(
+					'before'           => '<p class="bbp-forums-list-title"><span>' . __( 'Subforums', 'eighties-bbpress' ) . '</span></p><ul class="bbp-forums-list">',
+					'separator'        => false,
+					'show_topic_count' => false,
+					'show_reply_count' => false
+				);
 
-		<?php bbp_list_forums(); ?>
+				bbp_list_forums( $args );
+			?>
 
-		<?php do_action( 'bbp_theme_after_forum_sub_forums' ); ?>
+			<?php do_action( 'bbp_theme_after_forum_sub_forums' ); ?>
 
-		<?php bbp_forum_row_actions(); ?>
+			<?php bbp_forum_row_actions(); ?>
+		</li>
 
-	</li>
+		<li class="bbp-forum-counts bbp-counts">
+			<div class="bbp-count-item">
+				<span><?php _e( 'Topics', 'eighties-bbpress' ); ?></span>
+				<span class="bbp-count-number"><?php bbp_forum_topic_count(); ?></span>
+			</div>
+			<div class="bbp-count-item">
+				<span><?php _e( 'Replies', 'eighties-bbpress' ); ?></span>
+				<span class="bbp-count-number"><?php bbp_show_lead_topic() ? bbp_forum_reply_count() : bbp_forum_post_count(); ?></span>
+			</div>
+		</li>
 
-	<li class="bbp-forum-topic-count"><?php bbp_forum_topic_count(); ?></li>
-
-	<li class="bbp-forum-reply-count"><?php bbp_show_lead_topic() ? bbp_forum_reply_count() : bbp_forum_post_count(); ?></li>
-
-	<li class="bbp-forum-freshness">
-
-		<?php do_action( 'bbp_theme_before_forum_freshness_link' ); ?>
-
-		<?php bbp_forum_freshness_link(); ?>
-
-		<?php do_action( 'bbp_theme_after_forum_freshness_link' ); ?>
-
-		<p class="bbp-topic-meta">
-
-			<?php do_action( 'bbp_theme_before_topic_author' ); ?>
-
-			<span class="bbp-topic-freshness-author"><?php bbp_author_link( array( 'post_id' => bbp_get_forum_last_active_id(), 'size' => 14 ) ); ?></span>
-
-			<?php do_action( 'bbp_theme_after_topic_author' ); ?>
-
-		</p>
-	</li>
-
-</ul><!-- #bbp-forum-<?php bbp_forum_id(); ?> -->
+	</ul><!-- #bbp-forum-<?php bbp_forum_id(); ?> -->
+</div>
